d5635de59a94d7550b2037adffe970902478a48b
Mumtoz Valijonov <mumtoz.valijonov@gmail.com>
2025-03-01 15:28:15 +0100

    Add solution to move_zeroes problem

---
+++ src/bin/move_zeroes.rs
@@ -0,0 +1,19 @@
+struct Solution;
+
+fn main() {
+    let mut vec = vec![1, 0, 2, 3, 0, 4, 5, 5, 0, 5];
+    Solution::move_zeroes(&mut vec);
+    println!("{:?}", vec);
+}
+
+impl Solution {
+    pub fn move_zeroes(nums: &mut Vec<i32>) {
+        let mut write_idx = 0;
+        for i in 0..nums.len() {
+            if nums[i] != 0 {
+                (nums[write_idx], nums[i]) = (nums[i], nums[write_idx]);
+                write_idx += 1;
+            }
+        }
+    }
+}
