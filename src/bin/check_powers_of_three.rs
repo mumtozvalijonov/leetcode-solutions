2e91f8cca884b74aa64f18b5ed138568ad0deeda
Mumtoz Valijonov <mumtoz.valijonov@gmail.com>
2025-03-04 20:04:54 +0100

    Add solution to 1780

---
+++ src/bin/check_powers_of_three.rs
@@ -0,0 +1,44 @@
+// 1780. Check if Number is a Sum of Powers of Three
+
+struct Solution;
+
+fn main() {
+    println!("{}", Solution::check_powers_of_three(90));
+}
+
+impl Solution {
+    pub fn check_powers_of_three(n: i32) -> bool {
+        let mut n = n;
+
+        while n > 2 {
+            if n % 3 == 0 {
+                n /= 3;
+            } else if (n - 1) % 3 == 0 {
+                n = (n - 1) / 3;
+            } else {
+                break;
+            }
+        }
+        n == 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        assert_eq!(Solution::check_powers_of_three(12), true);
+    }
+
+    #[test]
+    fn test_example2() {
+        assert_eq!(Solution::check_powers_of_three(91), true);
+    }
+
+    #[test]
+    fn test_example3() {
+        assert_eq!(Solution::check_powers_of_three(21), false);
+    }
+}
